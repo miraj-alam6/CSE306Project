732cac51f7c1c9b889eb83a6c4d2f9264422c4a9
miraj-alam6 <miraj.alam@stonybrook.edu>
2016-11-06 18:51:25 -0500

    SJF is working
    SJF and FCFS are working. In command line -sjf and -fcfs are the
    arguments for the respective cases

--- nachos/kernel/userprog/SJFQueue.java
+++ nachos/kernel/userprog/SJFQueue.java
@@ -4,31 +4,45 @@ import nachos.Debug;
 import nachos.machine.NachosThread;
 
 public class SJFQueue implements UPList{
-
     @Override
     public UserThread getNextProcess() {
 	// TODO Auto-generated method stub
 	UserThread nextProcess = null;
+	Debug.println('+', "Reached here");
+	
+	//New Stuff start here
+	//First check if anything has not had its
+	//ticks left set yet so that it becomes the next thread to run.
+	for(int i = 0 ; i < userThreads.size(); i++){
+	    if(userThreads.get(i).getTicksLeft() <= -1){
+		nextProcess = userThreads.get(i);
+		return nextProcess;
+	    }
+	}
+	//New Stuff end here
+	/**/
+	
+	
+	//If you got here in the code that means ticks left have been calculated
+	//for all the user threads.
 	if(userThreads.size() == 0){
 	    return null;
 	}
-	if(NachosThread.currentThread() instanceof UserThread){
-	    if(userThreads.size() == 1 && 
-		NachosThread.currentThread() == userThreads.get(0)){
-		return nextProcess;
+	int smallestIndex = 0;
+	UserThread smallestTimeThread = userThreads.get(0);
+	for(int i = 1; i < userThreads.size(); i++){
+	    if (userThreads.get(i).getTicksLeft() < smallestTimeThread.getTicksLeft()){
+		smallestTimeThread = userThreads.get(i);
+		smallestIndex = i;
 	    }
-	    else if(NachosThread.currentThread() == userThreads.get(0)){
-		nextProcess = userThreads.get(1);
-	    }
-	    else{
-		
-		nextProcess = userThreads.get(0);
-	    }
-	    
+	}
+	if(NachosThread.currentThread() == smallestTimeThread){
+	    nextProcess = null;
 	}
 	else{
-	    nextProcess = userThreads.get(0);
+	    nextProcess = smallestTimeThread;
 	}
+	
 	return nextProcess;
     }
 
@@ -36,37 +50,19 @@ public class SJFQueue implements UPList{
     public void finishThread(int spaceID) {
 	// TODO Auto-generated method stub
 	for(int i =0; i < userThreads.size(); i++){
-	    	if(userThreads.get(i).space.getSpaceID() == spaceID){
-	    	    userThreads.remove(i);
-	    	    Debug.println('q', "In FCFS size is " + userThreads.size());
-	    	} 
-	}	
+	   if(userThreads.get(i).space.getSpaceID() == spaceID){
+	       userThreads.remove(i);
+	       Debug.println('q', "In FCFS size is " + userThreads.size());
+	   } 
+	    
+	}
     }
 
     @Override
     public void addProcess(UserThread uT) {
-	// TODO Auto-generated method stub
-	int count = 0;
-	while(count < userThreads.size())
-	{
-	    UserThread temp = userThreads.get(count);
-	    if(temp.getTicksLeft() < uT.getTicksLeft())
-	    {
-		count++;
-	    }
-	    else if( (temp.getTicksLeft() > uT.getTicksLeft()) && count == 0)
-	    {
-		userThreads.add(1,uT);
-		break;
-	    }
-	    else 
-	    {
-		userThreads.add(count+1,uT);
-		break;
-	    }
-	}
-	Debug.println('q', "In SJF size is " + userThreads.size());
-	
+	userThreads.add(uT);
+	Debug.println('+', "In FCFS size is " + userThreads.size());
     }
+
 
 }
