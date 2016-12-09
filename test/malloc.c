5f951745d5c30302022e0aa6b4fa199faf57ca3a
Kwun Yin Chan <kwunyin.chan@stonybrook.edu>
2016-12-08 19:56:55 -0500

    Changed malloc to some general psuedocode
    Previous code has many flaws
    
    Change-Id: Ibdb80b5e7125660b2a736461c195038ad3f00ee5

--- test/malloc.c
+++ test/malloc.c
@@ -13,118 +13,33 @@ struct memory_region {
 
 void *malloc(unsigned int size);
 {
+	//Check if size is less than or equal to 0 because this is invalid size request
 	
-	if(size <= 0)
-	{
-		
-	}
+	//Check if the heap has been started, If not extend heap_limit set firstfree to this then split the block if it's too big
+	//return allocated memory data
+	//set firstfree to the other part of the split block
 	
-	if(*heap_start == *heap_limit)
-	{
-		//Extend heap_limit
-		//*heap_limit += size;
-		firstfree = (struct memory_region *)heap_start;
-		firstfree->next = NULL;
-		firstfree->size = heap_limit - heap_start;
-		return firstfree;
-	}
-	else
-	{
-		struct memory_region *track = firstfree;
-		
-		while(track != NULL)
-		{
-			if(track->size >= size+sizeof(struct memory_region *))
-			{
-				break;
-			}
-			else
-			{
-				track = track->next;
-			}
-		}
-		
-		if(track == NULL)
-		{
-			//Extend heap_limit
-			
-		}
+	//else
+	//Find a free block that is big enough to complete the malloc
+	//If no free block big enough-> extend the heap_limit first
 	
-		struct memory_region *t2 = firstfree;
-		while(t2->next != track)
-		{
-			t2 = t2->next;
-		}
-		
-		if(track->size > size+sizeof(struct memory_region *))
-		{
-			struct memory_region *t3 = track;
-			t3 += size+sizeof(struct memory_region *);
-			t3->next = t2->next;
-			t2->next = t3;
-			return track;
-		}
-		else
-		{
-			track += size + sizeof(struct memory_region *);
-			return track;
-		}
-		
-	}
+	//if block is big enough and bigger than what we need -> split the block into a free region and allocated region
+	//update free list
+	//if block used is firstfree have to update firstfree
+	
+	//else if block is just the right size we need then allocate it and update freelist
+	//if block used is firstfree have to update firstfree
 }
 
 void free(void *ptr)
 {
-	struct memory_region *pp =
-            (struct memory_region *)(p - ((void *)((struct memory_region *)p)->data - p));
-			
-	//if pp exists
-	if(pp < heap_limit && pp > heap_start)
-	{
-		//Compare address to freelist
-		struct memory_region *prevPP = firstfree;
-		struct memory_region *nextPP = firstfree;
-		
-		if(pp < firstfree)
-		{
-			pp->next = firstfree->next;
-			firstfree = pp;
-			nextPP = pp->next;
-		}
-		else
-		{
-			//fit into correct spot by ascending order
-			struct memory_region *curr = firstfree;
-			
-			while(curr != NULL)
-			{
-				if(pp < curr)
-				{
-					pp->next = curr;
-					break;
-				}
-				curr = curr->next;
-			}
-			
-			curr = firstfree;
-			while(curr != NULL)
-			{
-				if(curr->next == pp->next)
-				{
-					break;
-				}
-				curr = curr->next;
-			}
-			
-			curr->next = pp;
-			
-			//check if the end of prev is contigious with the current address
-			//check if size+address of this to contingious of next address
-		}	
-	}
-	//else
-	else
-	{
-		//does not exist in the heap
-	}
+	//Check if pointer is valid
+	
+	//Free the pointer
+	
+	//Find the next freeblock closest to this
+	//Find the prev freeblock closest to this
+	//Add this block to freelist
+	//If this block is right next to the end/start of any freeblock coalesce
+	
 }	
